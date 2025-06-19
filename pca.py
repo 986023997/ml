3e315e589bb5e3a7a36d353ddfe7b1f9f6be6973
zhulin <oncwnuJJs2hJRxMyZ7G2Ol1XOv4A@git.weixin.qq.com>
2025-06-19 18:02:23 +0800

    pca

---
+++ pca.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+
+class pca:
+    def __init__(self, n_components):
+        assert n_components >= 1, "n_components must be valid"
+        self.n_components = n_components
+        self.components_ = None
+        
+    def demean(self, x):
+        return x - np.mean(x,axis = 0)
+
+    def f(self, w, x):
+        return np.sum((x.dot(w) ** 2)) / len(x)
+
+    
+    def df(self, w, x):
+        return x.T.dot(x.dot(w)) * 2 / len(x)
+
+
+    
+    def direction(w):
+        return w / np.linalg.norm(w)
+
+    
+    def first_component(self, x, initial_w, eta, n_iters = 1e4, epsilon = 1e-8):
+        w = direction(initial_w)
+        cur_iter = 0
+
+        while cur_iter < n_iters:
+            gradient = self.df(w,x)
+            last_w = w
+            w = w + eta * gradient
+            w = direction(w)
+            if(abs(self.f(w,x) - self.f(last_w,x)) < epsilon):
+                break
+            cur_iter += 1
+        return w
+    
+    
+    def fit(self, x, initial_w, eta, n_iters = 1e4, epsilon = 1e-8):
+        x_new = self.demean(x)
+        print(x_new)
+        self.components_ = np.empty(shape=(self.n_components,x.shape[1]))
+        
+        for i in range(self.n_components):
+            initial_w = np.random.random(x_new.shape[1])
+            w = first_component(x_new,initial_w,eta,n_iters,epsilon)
+            self.components_[i,:] = w
+            print(w)
+            print(x_new.dot(w))
+            x_new = x_new - x_new.dot(w).reshape([-1,1]) * w
+        return self
+    
+    
+    def transform(self, x):
+        assert x.shape[1] == self.components_.shape[1]
+        return x.dot(self.components_.T)
+    
+        
+    def inverse_transform(self, x):
+        assert x.shape[1] == self.components_.shape[1]
+        return x.dot(self.components_)

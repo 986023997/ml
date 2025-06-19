a12c53a3e7f0b9597e3b9094280d6822076683b1
Ciapge <44491712+986023997@users.noreply.github.com>
2025-06-19 23:43:36 +0800

    pca

--- .ipynb_checkpoints/pca-checkpoint.py
+++ .ipynb_checkpoints/pca-checkpoint.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class pca:
+class PCA:
     def __init__(self, n_components):
         assert n_components >= 1, "n_components must be valid"
         self.n_components = n_components
@@ -19,36 +19,33 @@ class pca:
 
 
     
-    def direction(w):
+    def direction(self,w):
         return w / np.linalg.norm(w)
 
     
     def first_component(self, x, initial_w, eta, n_iters = 1e4, epsilon = 1e-8):
-        w = direction(initial_w)
+        w = self.direction(initial_w)
         cur_iter = 0
 
         while cur_iter < n_iters:
             gradient = self.df(w,x)
             last_w = w
             w = w + eta * gradient
-            w = direction(w)
+            w = self.direction(w)
             if(abs(self.f(w,x) - self.f(last_w,x)) < epsilon):
                 break
             cur_iter += 1
         return w
     
     
-    def fit(self, x, initial_w, eta, n_iters = 1e4, epsilon = 1e-8):
+    def fit(self, x, eta = 0.01, n_iters = 1e4, epsilon = 1e-8):
         x_new = self.demean(x)
-        print(x_new)
         self.components_ = np.empty(shape=(self.n_components,x.shape[1]))
         
         for i in range(self.n_components):
             initial_w = np.random.random(x_new.shape[1])
-            w = first_component(x_new,initial_w,eta,n_iters,epsilon)
+            w = self.first_component(x_new,initial_w,eta,n_iters,epsilon)
             self.components_[i,:] = w
-            print(w)
-            print(x_new.dot(w))
             x_new = x_new - x_new.dot(w).reshape([-1,1]) * w
         return self
     
